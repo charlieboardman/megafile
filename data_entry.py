01398d6a5af3ec9358b0915284bf3b4f6a537430
Charlie Boardman <cboardman2@gmail.com>
2025-01-09 14:42:33 -0800

    Put together quite a bit of the main file, which required adding some more functions and retooling some functions in data_entry.py

--- data_entry.py
+++ data_entry.py
@@ -4,54 +4,47 @@ import csv
 def read_accounts(filepath='accounts.csv'):
     with open(filepath, 'r') as file:
         accounts = list(csv.reader(file))[1:]
-        return [f'{account_number} {full_name}' for [account_number,full_name,account_type,account_subtype,description] in accounts]
-    
+        return accounts
 def read_classes(filepath='classes.csv'):
     with open(filepath, 'r') as file:
         classes = list(csv.reader(file))[1:]
-        return [f'{class_number} {class_name}' for [class_number,class_name] in classes]
+        return classes
+def read_vendors(filepath='ledger.csv'):
+    with open(filepath, 'r') as file:
+        csv_reader = csv.DictReader(file)
+        vendors = [row['vendor'] for row in csv_reader]
+        return vendors
+def read_projects(filepath='projects.txt'):
+    with open(filepath, 'r') as file:
+        projects = [line.strip('\n') for line in file.readlines()]
+        return projects
 
 import prompt_toolkit
 from prompt_toolkit.completion import Completer, Completion
 from fuzzyfinder import fuzzyfinder
 
-def fuzzy_autocomplete_prompt(field, strings):
+def fuzzy_autocomplete_prompt(field, strings_for_autocomplete):
     class FuzzyCompleter(Completer):
         def get_completions(self, document, complete_event):
             # Get the entire line up to the cursor
             line_before_cursor = document.current_line_before_cursor
-
             # Find matches using fuzzyfinder
-            matches = fuzzyfinder(line_before_cursor, strings)
-
+            matches = fuzzyfinder(line_before_cursor, strings_for_autocomplete)
             # Yield Completion items for each match, replacing the entire line
             for m in matches:
                 # -len(line_before_cursor) will replace the whole line before the cursor
                 yield Completion(m, start_position=-len(line_before_cursor))
-
     user_input = prompt_toolkit.prompt(f'Enter {field}: ', completer=FuzzyCompleter())
     return user_input
 
 
-def receipt_id_generator(description,transaction_date,master_path):
+def receipt_id_generator(description,date_str,master_path):
     words = description.split()
     first_letters = [x[0] for x in words]
     acronym = ''.join(first_letters)
     if len(acronym) > 6:
-        acronym = acronym[0:8]
+        acronym = acronym[0:6]
         
-    #Parse the date
-    if isinstance(transaction_date,date):
-        date_str = transaction_date.strftime('%Y-%m-%d')
-        
-    elif type(transaction_date) == str:
-        date_str = parser.parse(transaction_date).strftime('%Y-%m-%d')
-        
-    else:
-        print("Bad date format")
-        return(0)
-    
-    
     #Read the last line of the master file
     #Modify this so that it works even if the file is empty
     with open(master_path,'r') as master_file:
@@ -59,6 +52,6 @@ def receipt_id_generator(description,transaction_date,master_path):
         same_date_transactions = [t.strip('\n') for t in transactions if t[0:10] == date_str]
         numeral = '{:02d}'.format(len(same_date_transactions)+1)
                 
-    final_string = f'{date_str}-{acronym}-{numeral}'
+    final_string = f'{date_str}-n{numeral}-{acronym}'
     
     return final_string
--- main.py
+++ main.py
@@ -1,5 +1,46 @@
 from capture_image import capture_image
 from datetime import date
+import data_entry
+from dateparser import parse
 
-today_str = date.today().isoformat()
+accounts = data_entry.read_accounts()
+accounts_strings_for_autocomplete = [f'{account_number} {full_name}' for [account_number,full_name,account_type,account_subtype,description] in accounts]
 
+classes = data_entry.read_classes()
+classes_strings_for_autocomplete = [f'{class_number} {class_name}' for [class_number,class_name] in classes]
+
+date = parse(input('Date: ')).date().isoformat()
+
+account_ = data_entry.fuzzy_autocomplete_prompt('account',accounts_strings_for_autocomplete)
+account_code = account_.split(' ')[0]
+account_name = ' '.join(account_.split(' ')[1:])
+
+class_ = data_entry.fuzzy_autocomplete_prompt('class',classes_strings_for_autocomplete)
+class_code = class_.split(' ')[0]
+class_name = ' '.join(class_.split(' ')[1:])
+
+mxn = input('mxn: ')
+try:
+    float(mxn)
+    ER = input('ER: ') #todo add error handling/validation for ER
+except:
+    ER = ''
+
+usd = input('usd: ')
+#todo handle this error better
+float(usd)
+
+calc_usd = "{:.2f}".format(float(usd)+float(mxn)/float(ER))
+#print(calc_usd)
+
+vendors = data_entry.read_vendors()
+vendor = data_entry.fuzzy_autocomplete_prompt('vendor',vendors)
+description = input('Description: ')
+notes = input('Notes: ')
+projects = data_entry.read_projects()
+project = data_entry.fuzzy_autocomplete_prompt('project',projects)
+print('Please capture receipt')
+receipt_pdf = capture_image()
+receipt_id = data_entry.receipt_id_generator(description,date,'ledger.csv')
+row = [date,account_code,account_name,class_code,class_name,mxn,ER,usd,calc_usd,vendor,description,notes,project,receipt_id]
+print(row)
