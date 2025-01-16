7522775889ada9fddcfe77f4cdb411fdf5fe9063
Charlie Boardman <cboardman2@gmail.com>
2025-01-15 17:16:18 -0800

    Changed the row variable to a dictionary

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ from datetime import date
 import data_entry
 from dateparser import parse
 
+row = dict()
+
 accounts = data_entry.read_accounts()
 accounts_strings_for_autocomplete = [f'{account_number} {full_name}' for [account_number,full_name,account_type,account_subtype,description] in accounts]
 
 classes = data_entry.read_classes()
 classes_strings_for_autocomplete = [f'{class_number} {class_name}' for [class_number,class_name] in classes]
 
-date = parse(input('Date: ')).date().isoformat()
+row['date'] = parse(input('Date: ')).date().isoformat()
 
 account_ = data_entry.fuzzy_autocomplete_prompt('account',accounts_strings_for_autocomplete)
-account_code = account_.split(' ')[0]
-account_name = ' '.join(account_.split(' ')[1:])
+row['account_code'] = account_.split(' ')[0]
+row['account_name'] = ' '.join(account_.split(' ')[1:])
 
 class_ = data_entry.fuzzy_autocomplete_prompt('class',classes_strings_for_autocomplete)
-class_code = class_.split(' ')[0]
-class_name = ' '.join(class_.split(' ')[1:])
+row['class_code'] = class_.split(' ')[0]
+row['class_name'] = ' '.join(class_.split(' ')[1:])
 
 mxn = input('mxn: ')
 try:
@@ -33,15 +35,17 @@ float(usd)
 calc_usd = "{:.2f}".format(float(usd)+float(mxn)/float(ER))
 #print(calc_usd)
 
+row['mxn'] = mxn; row['ER'] = ER; row['usd'] = usd; row['calc_usd'] = calc_usd
+
 vendors = data_entry.read_vendors()
-vendor = data_entry.fuzzy_autocomplete_prompt('vendor',vendors)
-description = input('Description: ')
-notes = input('Notes: ')
+row['vendor'] = data_entry.fuzzy_autocomplete_prompt('vendor',vendors)
+row['description'] = input('Description: ')
+row['notes'] = input('Notes: ')
 projects = data_entry.read_projects()
-project = data_entry.fuzzy_autocomplete_prompt('project',projects)
+row['project'] = data_entry.fuzzy_autocomplete_prompt('project',projects)
 print('Please capture receipt')
 receipt_pdf = capture_image() #todo need to save this to disk/drive
-receipt_id = data_entry.receipt_id_generator(description,date,'ledger.csv')
-#todo need to append this row to the real ledger/google sheet
-row = [date,account_code,account_name,class_code,class_name,mxn,ER,usd,calc_usd,vendor,description,notes,project,receipt_id]
+row['receipt_id'] = data_entry.receipt_id_generator(row['description'],row['date'],'ledger.csv')
+#todo need to append the row to the real ledger/google sheet
+
 print(row)
